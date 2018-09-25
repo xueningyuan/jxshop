85118b02eaaf5fefc9de1cc2dc4fde67954bdcac
fortheday <fortheday@126.com>
2018-09-25 08:49:18 +0800

    项目初始化

---
+++ controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+namespace controllers;
+
+class UserController
+{
+    public function hello()
+    {
+        echo 'user.hello';
+    }
+
+    public function li()
+    {
+        echo 'user.li';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+
+define('ROOT', __DIR__ . '/../');
+
+/**
+ * 类的自动加载
+ */
+function load($class)
+{
+    $path = str_replace('\\', '/', $class);
+    require(ROOT . $path . '.php');
+}
+spl_autoload_register('load');
+
+
+/**
+ * 解析路由
+ */
+$controller = '\controllers\IndexController';
+$action = 'index';
+if(isset($_SERVER['PATH_INFO']))
+{
+    //     0  1      2
+    //      /user/register
+    $router = explode('/', $_SERVER['PATH_INFO']);
+    $controller = '\controllers\\'.ucfirst($router[1]) . 'Controller';
+    $action = $router[2];
+}
+
+$c = new $controller;
+$c->$action();
