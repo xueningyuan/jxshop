3abbd5b41056bd6e3b2ae0c1fa2f8880ffe8a070
1transcend <41000757+1transcend@users.noreply.github.com>
2018-10-11 09:27:12 +0800

    Update db.sql

--- db.sql
+++ db.sql
@@ -185,3 +185,5 @@ insert into admin(id,username,password) VALUES
 (1,'root','21232f297a57a5a743894a0e4a801fc3'),
 (2,'tom','21232f297a57a5a743894a0e4a801fc3'),
 (3,'jack','21232f297a57a5a743894a0e4a801fc3');
+
+# 我是yinrui
